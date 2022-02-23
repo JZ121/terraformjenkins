e2c48c7e40ef418759bf429e6fe556daa167be52
JZ121 <jafar_jz@hotmail.com>
2022-02-23 20:10:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id   = "<azure_subscription_id>"
-  tenant_id         = "<azure_subscription_tenant_id>"
-  client_id         = "<service_principal_appid>"
-  client_secret     = "<service_principal_password>"
+  subscription_id   = "f5255551-d2be-4909-a91a-9e375ddd2912"
+  tenant_id         = "c16ce138-64f7-4f97-85a0-315c092080ad"
+  client_id         = "edfe62ed-2bea-4d92-9fd4-fa61c6236ca3"
+  client_secret     = "3tA45xS8MA~KCpI2CsFUu99hkBVPAGnv8~"
 }
 
 resource "azurerm_resource_group" "example" {
